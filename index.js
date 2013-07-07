831202e51250766dca524b49c234baebb5b683e5
Blaine Bublitz <blaine@iceddev.com>
2013-07-06 19:25:37 -0700

    code style

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var es = require('event-stream');
 var compile = require('jade').compile;
 var clone = require('clone');
 
-module.exports = function(options) {
-	'use strict';
+module.exports = function(options){
+  'use strict';
 
-	var opts = options ? clone(options) : {};
+  var opts = options ? clone(options) : {};
 
-	function jade(file, callback) {
-		var newFile = clone(file);
-		var compiled = compile(String(newFile.contents), opts);
-		var result = opts.client ? compiled.toString() : compiled(opts.data);
-		newFile.contents = new Buffer(result);
+  function jade(file, callback){
+    var newFile = clone(file);
+    var compiled = compile(String(newFile.contents), opts);
+    var result = opts.client ? compiled.toString() : compiled(opts.data);
+    newFile.contents = new Buffer(result);
 
-		callback(null, newFile);
-	}
+    callback(null, newFile);
+  }
 
-	return es.map(jade);
+  return es.map(jade);
 };
