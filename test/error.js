9be19a10189f7463e3583cbef990fc870ed4cb0c
Ivan Fraixedes <ifcdev@gmail.com>
2014-12-18 13:13:35 +0000

    Added test for `logErrors` option

--- test/error.js
+++ test/error.js
@@ -5,6 +5,7 @@ var test = require('tap').test;
 var gulp = require('gulp');
 var task = require('../');
 var path = require('path');
+var through = require('through2');
 
 var filename = path.join(__dirname, './fixtures/jade-error.jade');
 
@@ -16,4 +17,17 @@ test('should emit errors of jade correctly', function(t){
         t.ok(err instanceof Error);
         t.end();
       }));
+});
+
+test('should not emit errors of jade correctly when `logErrors` option is `true`', function(t){
+  gulp.src(filename)
+  .pipe(task({
+    logErrors: true
+  })
+  .on('error', function (){
+    t.notOk(false, 'Error is not expected when `logErrors` is true');
+  }))
+  .pipe(through.obj(function (){
+    t.end();
+  }));
 });
