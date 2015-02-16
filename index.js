ebab721ddefe6a66acfa0b8b1598b699fa7e40c7
Blaine Bublitz <blaine@iceddev.com>
2015-02-16 11:48:43 -0700

    cleanup, requires to the top, do not mutate the options passed in

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 'use strict';
 
+var extend = require('util')._extend;
+
 var through = require('through2');
+var defaultJade = require('jade');
 var ext = require('gulp-util').replaceExtension;
 var PluginError = require('gulp-util').PluginError;
 
-module.exports = function(opts){
-  opts = opts || {};
-  var jade = opts.jade || require('jade');
+module.exports = function(options){
+  var opts = extend({}, options);
+  var jade = opts.jade || defaultJade;
 
   function CompileJade(file, enc, cb){
     opts.filename = file.path;
 
-    if (file.data) {
+    if(file.data){
       opts.data = file.data;
     }
 
@@ -25,7 +28,7 @@ module.exports = function(opts){
       try {
         var compiled;
         var contents = String(file.contents);
-        if (opts.client) {
+        if(opts.client){
           compiled = jade.compileClient(contents, opts);
         } else {
           compiled = jade.compile(contents, opts)(opts.locals || opts.data);
