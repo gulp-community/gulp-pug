29c4a0e3b9e479fe2438d163f06ad6ce1156ff1a
Den <dddenisss35@gmail.com>
2016-01-19 21:44:16 +0200

    External template render function

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 'use strict';
 
 const through = require('through2');
-const defaultJade = require('jade');
 const ext = require('gulp-util').replaceExtension;
 const PluginError = require('gulp-util').PluginError;
-const path = require('path');
+const defaultJade = require('jade');
 
-
-function setPathName(_path, name) {
-  var parsed = path.parse(_path);
-  parsed.base = name;
-  return path.format( parsed );
-}
-function getPathName(_path) {
-  return path.parse(_path).base;
-}
 
 module.exports = function (options) {
   options = options || {};
   var jade = options.jade || defaultJade;
-  var pages = options.pages || {};
 
   return through.obj(function (file, enc, cb) {
     if (file.isNull()) {
@@ -55,26 +44,14 @@ module.exports = function (options) {
   }
 
   function compile(stream, file, data) {
-    var _pages = pages[getPathName(file.path)];
     var tpl = jade.compile(String(file.contents), options);
+    var render = options.render || defaultRender;
+    render.call(stream, tpl, data, file);
+  }
 
-    if (_pages === undefined) {
-      compileTemplate(tpl, data, file);
-    } else {
-      _pages.forEach((page, i) => {
-        var pageFile = file.clone();
-        pageFile.path = setPathName(pageFile.path, page);
-        compileTemplate(tpl, data, pageFile, i);
-      });
-    }
-
-    function compileTemplate(tpl, data, file, pageNum) {
-      data._file = file;
-      data._pageNum = pageNum;
-      file.contents = new Buffer(tpl(data));
-      stream.push(file);
-    }
-
+  function defaultRender(tpl, data, file) {
+    file.contents = new Buffer(tpl(data));
+    this.push(file);
   }
 
 };
