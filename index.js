d0ba4f4f647ee24ca0ca0b254bbd65935c08997b
Den <dddenisss35@gmail.com>
2016-03-11 18:16:14 +0200

    Refactoring

--- index.js
+++ index.js
@@ -7,51 +7,51 @@ const defaultJade = require('jade');
 
 
 module.exports = function (options) {
-  options = options || {};
-  var jade = options.jade || defaultJade;
+    options = options || {};
+    var jade = options.jade || defaultJade;
 
-  return through.obj(function (file, enc, cb) {
-    if (file.isNull()) {
-      return cb(null, file);
-    }
-    if (file.isStream()) {
-      return cb(new PluginError('gulp-jade', 'Streaming not supported'));
-    }
-
-    if (file.isBuffer()) {
-      options.filename = file.path;
-      file.path = ext(file.path, options.client ? '.js' : '.html');
-      var data = file.data || options.locals || options.data;
-
-      try {
-        if (options.client) {
-          compileClient(this, file);
-        } else {
-          compile(this, file, data);
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            return cb(null, file);
         }
-      } catch (e) {
-        return cb(new PluginError('gulp-jade', e));
-      }
+        if (file.isStream()) {
+            return cb(new PluginError('gulp-jade', 'Streaming not supported'));
+        }
 
-      cb();
+        if (file.isBuffer()) {
+            options.filename = file.path;
+            file.path = ext(file.path, options.client ? '.js' : '.html');
+
+            try {
+                if (options.client) {
+                    compileClient.call(this, file);
+                } else {
+                    var data = file.data || options.locals || options.data;
+                    compile.call(this, file, data);
+                }
+            } catch (e) {
+                return cb(new PluginError('gulp-jade', e));
+            }
+
+            cb();
+        }
+    });
+
+    function compileClient(file) {
+        var template = jade.compileClient(String(file.contents), options);
+        file.contents = new Buffer(template);
+        this.push(file);
     }
-  });
 
-  function compileClient(stream, file) {
-    var compiled = jade.compileClient(String(file.contents), options);
-    file.contents = new Buffer(compiled);
-    stream.push( file );
-  }
+    function compile(file, data) {
+        var template = jade.compile(String(file.contents), options);
+        var render = options.render || defaultRender;
+        render.call(this, file, template, data);
+    }
 
-  function compile(stream, file, data) {
-    var tpl = jade.compile(String(file.contents), options);
-    var render = options.render || defaultRender;
-    render.call(stream, tpl, data, file);
-  }
+}
 
-  function defaultRender(tpl, data, file) {
-    file.contents = new Buffer(tpl(data));
+function defaultRender(file, template, data) {
+    file.contents = new Buffer(template(data));
     this.push(file);
-  }
-
-};
+}
