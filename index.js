a750bd5082276e23fc356baa4a9cc80a52385334
Jamen Marzonie <jamenmarz+gh@gmail.com>
2016-05-09 23:49:19 -0600

    Add miscellaneous changes (#120)

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
 'use strict';
 
 var extend = require('util')._extend;
-
 var through = require('through2');
 var defaultPug = require('pug');
 var ext = require('gulp-util').replaceExtension;
 var PluginError = require('gulp-util').PluginError;
 
-module.exports = function(options){
-
+module.exports = function gulpPug(options) {
   var opts = extend({}, options);
   var pug = opts.pug || opts.jade || defaultPug;
 
-  function CompilePug(file, enc, cb){
+  return through.obj(function compilePug(file, enc, cb) {
     opts.filename = file.path;
 
-    if(file.data){
+    if (file.data) {
       opts.data = file.data;
     }
 
     file.path = ext(file.path, opts.client ? '.js' : '.html');
 
-    if(file.isStream()){
+    if (file.isStream()) {
       return cb(new PluginError('gulp-pug', 'Streaming not supported'));
     }
 
-    if(file.isBuffer()){
+    if (file.isBuffer()) {
       try {
         var compiled;
         var contents = String(file.contents);
-        if(opts.client){
+        if (opts.client) {
           compiled = pug.compileClient(contents, opts);
         } else {
           compiled = pug.compile(contents, opts)(opts.locals || opts.data);
         }
         file.contents = new Buffer(compiled);
-
-      } catch(e) {
+      } catch (e) {
         return cb(new PluginError('gulp-pug', e));
       }
     }
     cb(null, file);
-  }
-
-  return through.obj(CompilePug);
+  });
 };
--- test/error.js
+++ test/error.js
@@ -8,10 +8,10 @@ var path = require('path');
 
 var filename = path.join(__dirname, './fixtures/pug-error.pug');
 
-test('should emit errors of pug correctly', function(t){
+test('should emit errors of pug correctly', function(t) {
   gulp.src(filename)
     .pipe(task()
-      .on('error', function(err){
+      .on('error', function(err) {
         t.ok(err);
         t.ok(err instanceof Error);
         t.end();
