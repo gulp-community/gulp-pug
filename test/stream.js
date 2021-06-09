642d1ea8088dd14162a6ccb767ebf8fbda690456
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-06-08 20:50:25 -0700

    feat: Add streaming support using vinyl-contents (#200)

--- test/stream.js
+++ test/stream.js
@@ -4,26 +4,56 @@ const expect = require('expect');
 const fs = require('fs');
 const { concat, from, pipe } = require('mississippi');
 const path = require('path');
-const PluginError = require('plugin-error');
 const Vinyl = require('vinyl');
-const task = require('../');
+const pug = require('pug');
+
+const plugin = require('../');
 
 const cwd = __dirname;
 const base = path.join(cwd, 'fixtures');
 const filePath = path.join(base, 'helloworld.pug');
+const fileContents = fs.readFileSync(filePath);
 
-const file = new Vinyl({
-  path: filePath,
-  base: base,
-  cwd: cwd,
-  contents: fs.createReadStream(filePath),
-});
 
-describe('stream', function () {
-  it('should error if contents is a stream', function (done) {
-    pipe([from.obj([file]), task(), concat()], (err) => {
-      expect(err).toBeInstanceOf(PluginError);
-      done();
+describe('stream', function() {
+  it('should handle streaming contents', function(done) {
+    const file = new Vinyl({
+      path: filePath,
+      base: base,
+      cwd: cwd,
+      contents: fs.createReadStream(filePath),
     });
+
+    function assert(files) {
+      var expected = pug.compile(fileContents)();
+      expect(files[0]).not.toBeUndefined();
+      expect(expected).toEqual(String(files[0].contents));
+    }
+
+    pipe([
+      from.obj([file]),
+      plugin(),
+      concat(assert),
+    ], done);
+  });
+
+  it('bubbles errors if the stream errors', function(done) {
+    const file = new Vinyl({
+      path: filePath,
+      base: base,
+      cwd: cwd,
+      contents: from([new Error("Boom")])
+    });
+
+    function assert(err) {
+      expect(err).toBeDefined();
+      done();
+    }
+
+    pipe([
+      from.obj([file]),
+      plugin(),
+      concat(),
+    ], assert);
   });
 });
